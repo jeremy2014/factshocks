336603e2c40f67afcf9fc7a98c5d5881c65fd284
Jeremiah Sebastian Samatra <jeremiah_cool01@yahoo.com>
2013-12-18 16:30:30 +0800

    factshot sample only

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Factshocks::Application.config.secret_key_base = 'aa9673db68ae8336635b18eb8b2b7875a7a4f51d906317e383450e03a3ca3a72d0068a61fe4e6b3894ba397a2fb238121d9440da5c3d736522d0a137357f2bc6'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Factshocks::Application.config.session_store :cookie_store, key: '_factshocks_session'
